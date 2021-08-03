ed02ae3d4be112c8337cbaa2d17f5b13527e9c95
Joao Victor <joaogmavictor@gmail.com>
2021-08-03 12:26:47 -0300

    Inciando Components

--- stopwatch-challenge/src/App.js
+++ stopwatch-challenge/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
+import Timer from './Components/Temporizador';
 import './App.css';
 
 function App() {
   return (
-    <main>
-      <header className="app-header">
-      </header>
-      <section className="main-section">
-        <div className="action-buttons-div">
-
-        </div>
-        <div className="timer-div">
-
-        </div>
-        <div className="setup-div">
-
-        </div>
-      </section>
-    </main>
+    <div>
+      <Timer />
+    </div>
   );
 }
 
---
+++ stopwatch-challenge/src/Components/Temporizador.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+  render() {
+    return (
+      <div className="timer-div">
+        <button />
+        <button />
+        <button />
+      </div>
+    );
+  }
+}
+
+export default Timer;
