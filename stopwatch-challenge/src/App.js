3255342ace1995da92281268768f97d5d79a2002
alanmdf <alanmoreirascr@gmail.com>
2021-08-03 12:38:55 -0300

    Criaçao dos botoes de açao

--- stopwatch-challenge/src/App.js
+++ stopwatch-challenge/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+
 import Timer from './Components/Temporizador';
+import ActionButtons from './Components/ActionButtons';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Timer />
-    </div>
+    <main>
+      <header className="app-header">
+        👻 GhostDevs Trybe Stopwatch 👻
+      </header>
+      <section className="main-section">
+        <ActionButtons className="action-buttons" />
+        <div className="timer-div">
+
+        </div>
+        <div className="setup-div">
+
+        </div>
+      </section>
+    </main>
   );
 }
 
