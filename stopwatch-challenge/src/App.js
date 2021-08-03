a138cc992ded61a717986f4fe7cef04466954720
Joao Victor <joaogmavictor@gmail.com>
2021-08-03 12:35:17 -0300

    Terminando Componente Timer

--- stopwatch-challenge/src/App.js
+++ stopwatch-challenge/src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Timer />
-    </div>
+    <main>
+      <header className="app-header" />
+      <section className="main-section">
+        <div className="action-buttons-div" />
+        <div className="timer-div" />
+        <div className="setup-div" />
+      </section>
+    </main>
   );
 }
 
