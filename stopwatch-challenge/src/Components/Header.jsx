9a06b5364d369fd198428d9087c2b55a8cd990ca
Joao Victor <joaogmavictor@gmail.com>
2021-08-03 13:10:18 -0300

    Instalando Font Awesome

--- stopwatch-challenge/src/Components/Header.jsx
+++ stopwatch-challenge/src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import icons from './Icons/LibraryIcons';
 
 class Header extends Component {
   render() {
     return (
       <header className="app-header">
         👻 GhostDevs Trybe Stopwatch 👻
+        {icons.setigins}
       </header>
     );
   }
--- stopwatch-challenge/src/Components/Icons/LibraryIcons.js
+++ stopwatch-challenge/src/Components/Icons/LibraryIcons.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { faCog, faBars } from '@fortawesome/free-solid-svg-icons';
 import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const icons = {
-  setigins: faCog,
+  setigins: <FontAwesomeIcon icon={ faCog } />,
   menu: faBars,
   gitHub: faGithubAlt,
   linkedin: faLinkedinIn,
