6d69e91a8cbb0fc21bf370bb99167f8a35961dc6
Joao Victor <joaogmavictor@gmail.com>
2021-08-03 13:01:49 -0300

    Adicionando Library Icons

---
+++ stopwatch-challenge/src/Components/Icons/LibraryIcons.js
@@ -0,0 +1,11 @@
+import { faCog, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+const icons = {
+  setigins: faCog,
+  menu: faBars,
+  gitHub: faGithubAlt,
+  linkedin: faLinkedinIn,
+};
+
+export default icons;
